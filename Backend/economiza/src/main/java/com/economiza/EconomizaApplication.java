7c2cd7e7281c9e547467f31a63484c96a11fc829
Mario Valdivieso <g10vann1vch@gmail.com>
2025-05-02 10:46:39 -0500

    Backend initializr implementado

---
+++ Backend/economiza/src/main/java/com/economiza/EconomizaApplication.java
@@ -0,0 +1,13 @@
+package com.economiza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomizaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomizaApplication.class, args);
+	}
+
+}
---
+++ Backend/economiza/src/test/java/com/economiza/EconomizaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.economiza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomizaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
