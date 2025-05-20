362c4f636aa8f4bf2a745e14fc62d228c4cae47c
Mario Valdivieso <g10vann1vch@gmail.com>
2025-05-20 01:19:00 -0500

    List users and Get user request implemented

--- Backend/economiza/src/main/java/com/economiza/controllers/AppUserController.java
+++ Backend/economiza/src/main/java/com/economiza/controllers/AppUserController.java
@@ -1,14 +1,15 @@
 package com.economiza.controllers;
 
+import com.economiza.dtos.AppUserDTO;
 import com.economiza.dtos.NewUserDTO;
 import com.economiza.entities.AppUser;
 import com.economiza.serviceinterfaces.IAppUserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -18,10 +19,23 @@ public class AppUserController {
 
     @PostMapping
     public void newUser(@RequestBody NewUserDTO nu){
-        System.out.println("DEBUG: NewUserDTO received. first_name: " + nu.getFirst_name() + nu.getLast_name() + nu.getMail() + nu.getPassword());
         ModelMapper m = new ModelMapper();
         AppUser au = m.map(nu, AppUser.class);
-        System.out.println("DEBUG: NewUserDTO received. first_name: " + au.getFirst_name() + au.getLast_name() + au.getMail() + au.getPassword());
         AUS.insert(au);
+    }
+
+    @GetMapping
+    public List<AppUserDTO> listUsers(){
+        return AUS.list().stream().map(y->{
+            ModelMapper m = new ModelMapper();
+            return m.map(y, AppUserDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public AppUserDTO getUser(@PathVariable("id") Integer id){
+        ModelMapper m = new ModelMapper();
+        AppUserDTO dto = m.map(AUS.listId(id), AppUserDTO.class);
+        return dto;
     }
 }
